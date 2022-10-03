7b1712b882c9fedd82a60614f3bcdf9189947789
acr0matic <work.acr0matic@gmail.com>
2022-10-03 15:20:40 +0300

    feat: верстка первых 4х секций

--- src/scripts/init.js
+++ src/scripts/init.js
@@ -1,10 +1,11 @@
 const lazyLoadInstance = new LazyLoad({
-  elements_selector: '.lazy__item',
+  elements_selector: '.lazy__item, .lazy-item',
 
   callback_loaded: (trigger) => {
     const container = trigger.closest('.lazy');
-    const loader = container.querySelector('.preloader');
-
-    if (loader) loader.classList.add('preloader--hide');
+    if (container) {
+      const loader = container.querySelector('.preloader');
+      if (loader) loader.classList.add('preloader--hide');
+    }
   },
 });
