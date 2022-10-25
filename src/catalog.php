fbe2979291693c9dfc2b3a3a9b7aac3ca1ef2237
acr0matic <work.acr0matic@gmail.com>
2022-10-25 22:57:18 +0300

    feat: страница "номера"

---
+++ src/catalog.php
@@ -0,0 +1,280 @@
+<?php include_once('layout/partial/header.php') ?>
+
+<!-- Основной контент -->
+<main id="catalog">
+  <section id="catalog-list" class="section">
+    <div class="container">
+      <div class="section__header">
+        <h1 class="section__title section__title--small">
+          Номера Отеля
+        </h1>
+      </div>
+      <!-- /.section__header -->
+
+      <div class="catalog-list">
+        <div class="row gy-4">
+          <div class="col-12 col-sm-6 col-lg-4 col-xl-3">
+            <div class="catalog-card">
+              <picture class="catalog-card__picture">
+                <img class="image image--cover" src="content/catalog/card/1.jpg" alt="">
+              </picture>
+
+              <div class="catalog-card__wrapper">
+                <div class="catalog-card__content">
+                  <h3 class="catalog-card__title">Номер 3</h3>
+                  <div class="catalog-card__price">от 1900 руб/сутки</div>
+                </div>
+                <!-- /.catalog-card__content -->
+
+                <div class="catalog-card__people catalog-card__list">
+                  <div>
+                    <img src="content/icon/people.svg" alt="">
+                    на 15 персон
+                  </div>
+                </div>
+                <!-- /.catalog-card__people -->
+
+                <div class="catalog-card__action">
+                  <a href="" class="catalog-card__button button button--block button-secondary">Смотреть номер</a>
+                </div>
+                <!-- /.catalog-card__action -->
+              </div>
+              <!-- /.catalog-card__wrapper -->
+            </div>
+            <!-- /.catalog-card -->
+          </div>
+          <!-- /.col-12 col-sm-6 col-lg-4 col-xl-3 -->
+
+          <div class="col-12 col-sm-6 col-lg-4 col-xl-3">
+            <div class="catalog-card">
+              <picture class="catalog-card__picture">
+                <img class="image image--cover" src="content/catalog/card/1.jpg" alt="">
+              </picture>
+
+              <div class="catalog-card__wrapper">
+                <div class="catalog-card__content">
+                  <h3 class="catalog-card__title">Номер 3</h3>
+                  <div class="catalog-card__price">от 1900 руб/сутки</div>
+                </div>
+                <!-- /.catalog-card__content -->
+
+                <div class="catalog-card__people catalog-card__list">
+                  <div>
+                    <img src="content/icon/people.svg" alt="">
+                    на 15 персон
+                  </div>
+                </div>
+                <!-- /.catalog-card__people -->
+
+                <div class="catalog-card__action">
+                  <a href="" class="catalog-card__button button button--block button-secondary">Смотреть номер</a>
+                </div>
+                <!-- /.catalog-card__action -->
+              </div>
+              <!-- /.catalog-card__wrapper -->
+            </div>
+            <!-- /.catalog-card -->
+          </div>
+          <!-- /.col-12 col-sm-6 col-lg-4 col-xl-3 -->
+
+          <div class="col-12 col-sm-6 col-lg-4 col-xl-3">
+            <div class="catalog-card">
+              <picture class="catalog-card__picture">
+                <img class="image image--cover" src="content/catalog/card/1.jpg" alt="">
+              </picture>
+
+              <div class="catalog-card__wrapper">
+                <div class="catalog-card__content">
+                  <h3 class="catalog-card__title">Номер 3</h3>
+                  <div class="catalog-card__price">от 1900 руб/сутки</div>
+                </div>
+                <!-- /.catalog-card__content -->
+
+                <div class="catalog-card__people catalog-card__list">
+                  <div>
+                    <img src="content/icon/people.svg" alt="">
+                    на 15 персон
+                  </div>
+                </div>
+                <!-- /.catalog-card__people -->
+
+                <div class="catalog-card__action">
+                  <a href="" class="catalog-card__button button button--block button-secondary">Смотреть номер</a>
+                </div>
+                <!-- /.catalog-card__action -->
+              </div>
+              <!-- /.catalog-card__wrapper -->
+            </div>
+            <!-- /.catalog-card -->
+          </div>
+          <!-- /.col-12 col-sm-6 col-lg-4 col-xl-3 -->
+
+          <div class="col-12 col-sm-6 col-lg-4 col-xl-3">
+            <div class="catalog-card">
+              <picture class="catalog-card__picture">
+                <img class="image image--cover" src="content/catalog/card/1.jpg" alt="">
+              </picture>
+
+              <div class="catalog-card__wrapper">
+                <div class="catalog-card__content">
+                  <h3 class="catalog-card__title">Номер 3</h3>
+                  <div class="catalog-card__price">от 1900 руб/сутки</div>
+                </div>
+                <!-- /.catalog-card__content -->
+
+                <div class="catalog-card__people catalog-card__list">
+                  <div>
+                    <img src="content/icon/people.svg" alt="">
+                    на 15 персон
+                  </div>
+                </div>
+                <!-- /.catalog-card__people -->
+
+                <div class="catalog-card__action">
+                  <a href="" class="catalog-card__button button button--block button-secondary">Смотреть номер</a>
+                </div>
+                <!-- /.catalog-card__action -->
+              </div>
+              <!-- /.catalog-card__wrapper -->
+            </div>
+            <!-- /.catalog-card -->
+          </div>
+          <!-- /.col-12 col-sm-6 col-lg-4 col-xl-3 -->
+
+          <div class="col-12 col-sm-6 col-lg-4 col-xl-3">
+            <div class="catalog-card">
+              <picture class="catalog-card__picture">
+                <img class="image image--cover" src="content/catalog/card/1.jpg" alt="">
+              </picture>
+
+              <div class="catalog-card__wrapper">
+                <div class="catalog-card__content">
+                  <h3 class="catalog-card__title">Номер 3</h3>
+                  <div class="catalog-card__price">от 1900 руб/сутки</div>
+                </div>
+                <!-- /.catalog-card__content -->
+
+                <div class="catalog-card__people catalog-card__list">
+                  <div>
+                    <img src="content/icon/people.svg" alt="">
+                    на 15 персон
+                  </div>
+                </div>
+                <!-- /.catalog-card__people -->
+
+                <div class="catalog-card__action">
+                  <a href="" class="catalog-card__button button button--block button-secondary">Смотреть номер</a>
+                </div>
+                <!-- /.catalog-card__action -->
+              </div>
+              <!-- /.catalog-card__wrapper -->
+            </div>
+            <!-- /.catalog-card -->
+          </div>
+          <!-- /.col-12 col-sm-6 col-lg-4 col-xl-3 -->
+
+          <div class="col-12 col-sm-6 col-lg-4 col-xl-3">
+            <div class="catalog-card">
+              <picture class="catalog-card__picture">
+                <img class="image image--cover" src="content/catalog/card/1.jpg" alt="">
+              </picture>
+
+              <div class="catalog-card__wrapper">
+                <div class="catalog-card__content">
+                  <h3 class="catalog-card__title">Номер 3</h3>
+                  <div class="catalog-card__price">от 1900 руб/сутки</div>
+                </div>
+                <!-- /.catalog-card__content -->
+
+                <div class="catalog-card__people catalog-card__list">
+                  <div>
+                    <img src="content/icon/people.svg" alt="">
+                    на 15 персон
+                  </div>
+                </div>
+                <!-- /.catalog-card__people -->
+
+                <div class="catalog-card__action">
+                  <a href="" class="catalog-card__button button button--block button-secondary">Смотреть номер</a>
+                </div>
+                <!-- /.catalog-card__action -->
+              </div>
+              <!-- /.catalog-card__wrapper -->
+            </div>
+            <!-- /.catalog-card -->
+          </div>
+          <!-- /.col-12 col-sm-6 col-lg-4 col-xl-3 -->
+
+          <div class="col-12 col-sm-6 col-lg-4 col-xl-3">
+            <div class="catalog-card">
+              <picture class="catalog-card__picture">
+                <img class="image image--cover" src="content/catalog/card/1.jpg" alt="">
+              </picture>
+
+              <div class="catalog-card__wrapper">
+                <div class="catalog-card__content">
+                  <h3 class="catalog-card__title">Номер 3</h3>
+                  <div class="catalog-card__price">от 1900 руб/сутки</div>
+                </div>
+                <!-- /.catalog-card__content -->
+
+                <div class="catalog-card__people catalog-card__list">
+                  <div>
+                    <img src="content/icon/people.svg" alt="">
+                    на 15 персон
+                  </div>
+                </div>
+                <!-- /.catalog-card__people -->
+
+                <div class="catalog-card__action">
+                  <a href="" class="catalog-card__button button button--block button-secondary">Смотреть номер</a>
+                </div>
+                <!-- /.catalog-card__action -->
+              </div>
+              <!-- /.catalog-card__wrapper -->
+            </div>
+            <!-- /.catalog-card -->
+          </div>
+          <!-- /.col-12 col-sm-6 col-lg-4 col-xl-3 -->
+
+          <div class="col-12 col-sm-6 col-lg-4 col-xl-3">
+            <div class="catalog-card">
+              <picture class="catalog-card__picture">
+                <img class="image image--cover" src="content/catalog/card/1.jpg" alt="">
+              </picture>
+
+              <div class="catalog-card__wrapper">
+                <div class="catalog-card__content">
+                  <h3 class="catalog-card__title">Номер 3</h3>
+                  <div class="catalog-card__price">от 1900 руб/сутки</div>
+                </div>
+                <!-- /.catalog-card__content -->
+
+                <div class="catalog-card__people catalog-card__list">
+                  <div>
+                    <img src="content/icon/people.svg" alt="">
+                    на 15 персон
+                  </div>
+                </div>
+                <!-- /.catalog-card__people -->
+
+                <div class="catalog-card__action">
+                  <a href="" class="catalog-card__button button button--block button-secondary">Смотреть номер</a>
+                </div>
+                <!-- /.catalog-card__action -->
+              </div>
+              <!-- /.catalog-card__wrapper -->
+            </div>
+            <!-- /.catalog-card -->
+          </div>
+          <!-- /.col-12 col-sm-6 col-lg-4 col-xl-3 -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.catalog-list -->
+    </div>
+    <!-- /.container -->
+  </section>
+</main>
+
+<?php include_once('layout/partial/footer.php') ?>
